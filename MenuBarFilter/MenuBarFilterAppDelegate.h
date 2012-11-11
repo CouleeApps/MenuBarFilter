fd7720f7a5ac7094dc4000559c71e322c0a05ca5
Wez Furlong <wez@w.(none)>
2012-11-11 03:48:45 -0800

    More thoroughly fix the lion fullscreen flicker issue by observing more appropriate events. Take this further by watching for mission control events using undocumented APIs on Mountain Lion. Use a hopefully more robust approach for figuring out the menu bar height, as I had a report that it was too short on a retina MBP

--- MenuBarFilter/MenuBarFilterAppDelegate.h
+++ MenuBarFilter/MenuBarFilterAppDelegate.h
@@ -9,9 +9,9 @@
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
- 
+
  http://www.apache.org/licenses/LICENSE-2.0
- 
+
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,15 +23,25 @@
 
 @interface MenuBarFilterAppDelegate : NSObject <NSApplicationDelegate> {
 
-    IBOutlet NSMenu *statusMenu;    
-    
+    IBOutlet NSMenu *statusMenu;
+
 @private
     MenuBarFilterWindow * invertWindow;
     MenuBarFilterWindow * hueWindow;
     BOOL visible;
+#if USE_EVENT_TAP
+    CFMachPortRef eventTap;
+#endif
+    BOOL inMissionControl;
+    id eventMonitor;
     NSStatusItem *statusItem;
 }
 
 - (void) reposition;
+- (void) missionControlActivated;
+- (void) missionControlTapped;
+- (void) checkForFullScreen:(NSNotification*)notification;
+- (void) fullScreenShowMenuBar:(NSNotification*)notification;
+- (void) fullScreenHideMenuBar:(NSNotification*)notification;
 
 @end
--- MenuBarFilter/MenuBarFilterAppDelegate.m
+++ MenuBarFilter/MenuBarFilterAppDelegate.m
@@ -9,9 +9,9 @@
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
- 
+
  http://www.apache.org/licenses/LICENSE-2.0
- 
+
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,6 +26,31 @@
 NSString *window_server = @"Window Server";
 NSString *backstop_menubar = @"Backstop Menubar";
 
+// Undocumented spaces related APIs
+typedef int CGSConnection;
+extern CGSConnection _CGSDefaultConnection(void);
+typedef void (*CGConnectionNotifyProc)(int data1, int data2, int data3, void* userParameter);
+extern CGError CGSRegisterConnectionNotifyProc(const CGSConnection cid,
+                                               CGConnectionNotifyProc function, int event, void* userParameter);
+
+/* observed that this is sent when Mission Control is activated */
+#define CGSConnectionNotifyEventMissionControl 1204
+
+static void spaces_callback(int data1, int data2, int data3, void *ptr)
+{
+    MenuBarFilterAppDelegate *self = ptr;
+
+    switch (data1) {
+        case CGSConnectionNotifyEventMissionControl: // Mission Control
+            //NSLog(@"space_callback: Mission Control launched");
+            [self missionControlActivated];
+            break;
+
+        default:
+            NSLog(@"space_callback! ptr=%p %d %d %d", ptr, data1, data2, data3);
+    }
+}
+
 - (void) enableMenuItem:(BOOL)enable {
     if (statusItem && !enable) {
         [[statusItem statusBar] removeStatusItem:statusItem];
@@ -37,95 +62,154 @@ NSString *backstop_menubar = @"Backstop Menubar";
         [statusItem setImage:[NSImage imageNamed:@"NocturneMenu"]];
         [statusItem setAlternateImage:[NSImage imageNamed:@"NocturneMenuPressed"]];
         [statusItem retain];
-    }    
+    }
 }
+
+#if USE_EVENT_TAP
+static CGEventRef tap_callback (
+                                CGEventTapProxy proxy,
+                                CGEventType type,
+                                CGEventRef event,
+                                void *refcon
+                                )
+{
+    MenuBarFilterAppDelegate *self = refcon;
+    [self missionControlTapped];
+//    NSLog(@"MC tap %@", event);
+    return NULL;
+}
+#endif
 
 - (void) applicationDidFinishLaunching:(NSNotification *)notification {
 
     NSUserDefaults *defs = [NSUserDefaults standardUserDefaults];
-    
+
     NSDictionary *appDefaults = [NSMutableDictionary dictionary];
-    
+
     // defaults write org.wezfurlong.MenuBarFilter enableMenu NO
     [appDefaults setValue:[NSNumber numberWithBool:YES] forKey:@"enableMenu"];
-    
+
     // defaults write org.wezfurlong.MenuBarFilter useHue NO
     [appDefaults setValue:[NSNumber numberWithBool:YES] forKey:@"useHue"];
-                                 
+
     [defs registerDefaults:appDefaults];
-    
+
     [self enableMenuItem:[defs boolForKey:@"enableMenu"]];
-    
+
     // create invert overlay
     invertWindow = [[MenuBarFilterWindow alloc] init];
     [invertWindow setFilter:@"CIColorInvert"];
-    
+
     hueWindow = [[MenuBarFilterWindow alloc] init];
     if ([defs boolForKey:@"useHue"]) {
         // create hue overlay
         [hueWindow setFilter:@"CIHueAdjust"];
         [hueWindow setFilterValues:
-             [NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithFloat:M_PI], 
-              @"inputAngle", nil]];  
+             [NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithFloat:M_PI],
+              @"inputAngle", nil]];
     } else {
         // de-saturation filter
         [hueWindow setFilter:@"CIColorControls"];
         [hueWindow setFilterValues:
-            [NSDictionary dictionaryWithObject: [NSNumber numberWithFloat:0.0] 
+            [NSDictionary dictionaryWithObject: [NSNumber numberWithFloat:0.0]
             forKey: @"inputSaturation" ] ];
         [hueWindow setFilterValues:
-            [NSDictionary dictionaryWithObject: [NSNumber numberWithFloat:0.10] 
+            [NSDictionary dictionaryWithObject: [NSNumber numberWithFloat:0.10]
             forKey: @"inputBrightness" ] ];
         [hueWindow setFilterValues:
-            [NSDictionary dictionaryWithObject: [NSNumber numberWithFloat:0.8] 
-            forKey: @"inputContrast" ] ];        
+            [NSDictionary dictionaryWithObject: [NSNumber numberWithFloat:0.8]
+            forKey: @"inputContrast" ] ];
     }
-    
+
     // add observer for screen changes
-    [[NSNotificationCenter defaultCenter] addObserver:self 
-                                             selector:@selector(reposition) 
-                                                 name:NSApplicationDidChangeScreenParametersNotification 
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(reposition)
+                                                 name:NSApplicationDidChangeScreenParametersNotification
                                                object:nil];
-    
-    // add observer for full-screen
+
+    NSNotificationCenter *nc = [[NSWorkspace sharedWorkspace] notificationCenter];
+
+    // observe space/workspace changes, including Lion full-screen mode changes
+    [nc addObserver:self
+           selector:@selector(checkForFullScreen:)
+                                                 name:@"NSWorkspaceActiveSpaceDidChangeNotification"
+                                               object:nil];
+
+
+    // add observer for full-screen (not Lion style)
     [[NSApplication sharedApplication] addObserver:self
                                         forKeyPath:@"currentSystemPresentationOptions"
                                            options:( NSKeyValueObservingOptionOld | NSKeyValueObservingOptionNew )
                                            context:NULL];
-    
-    if ( [invertWindow respondsToSelector:@selector(toggleFullScreen:)] ) {                                                    
-        // lion hack
-        NSTimer * timer = [NSTimer timerWithTimeInterval:1
-                                                  target:self 
-                                                selector:@selector(checkForFullscreen) 
-                                                userInfo:nil 
-                                                 repeats:YES];
-        [[NSRunLoop mainRunLoop] addTimer:timer 
-                                  forMode:NSDefaultRunLoopMode];
-        
-        NSTimer * timer2 = [NSTimer timerWithTimeInterval:.1
-                                                  target:self 
-                                                selector:@selector(checkForAppSwitch) 
-                                                userInfo:nil 
-                                                 repeats:YES];
-        [[NSRunLoop mainRunLoop] addTimer:timer2
-                                  forMode:NSDefaultRunLoopMode];
+
+    // When mission control launches, if it is canceled using the swipe down gesture,
+    // we have no way to tell that it was canceled.  So we need to snoop on mouse
+    // events and re-show our filter.  My first attempt was using a quartz event tap
+    // but that receives input event while mission control is active.  So I switched
+    // to an NSEvent based monitor instead.  I've kept the tap code here just in case
+    // I need to bring it back again.
+#if USE_EVENT_TAP
+    eventTap = CGEventTapCreate(kCGAnnotatedSessionEventTap, kCGHeadInsertEventTap,
+                                kCGEventTapOptionListenOnly,
+                                CGEventMaskBit(kCGEventMouseMoved) |
+                                CGEventMaskBit(kCGEventKeyUp) | CGEventMaskBit(kCGEventKeyDown),
+                                tap_callback,
+                                self);
+
+    CFRunLoopAddSource(
+                       [[NSRunLoop currentRunLoop] getCFRunLoop],
+                       CFMachPortCreateRunLoopSource(NULL, eventTap, 0),
+                       kCFRunLoopCommonModes);
+    CGEventTapEnable(eventTap, NO);
+#endif
+
+    // When full-screen apps hide/show the menu bar
+    NSNotificationCenter *dc = [NSDistributedNotificationCenter defaultCenter];
+    [dc addObserver:self selector:@selector(fullScreenShowMenuBar:) name:@"com.apple.HIToolbox.frontMenuBarShown" object:nil];
+    [dc addObserver:self selector:@selector(fullScreenHideMenuBar:) name:@"com.apple.HIToolbox.hideMenuBarShown" object:nil];
+
+#if 0
+    // I used this to spy on the various notifications
+    [nc addObserver:self selector:@selector(GlobalNotificationObserver:) name:NULL object:nil];
+
+    // @"com.apple.HIToolbox.frontMenuBarShown"
+    // @"com.apple.HIToolbox.hideMenuBarShown"
+    [[NSDistributedNotificationCenter defaultCenter] addObserver:self selector:@selector(GlobalNotificationObserver:) name:NULL object:nil];
+#endif
+
+    // Use undocumented APIs to detect mission control being launched :-/
+    // I observed this event code on 10.8. No idea if this is valid for other versions of OSX, YMMV.
+    CGSRegisterConnectionNotifyProc(_CGSDefaultConnection(), spaces_callback, CGSConnectionNotifyEventMissionControl, self);
+
+#if 0
+    // I used this to spy on the spaces events
+    for (int i = 0; i < 2048; i++) {
+        CGSRegisterConnectionNotifyProc(_CGSDefaultConnection(), spaces_callback, i, self);
     }
-    
+#endif
+
     // show overlays
     [self reposition];
     [invertWindow orderFront:nil];
-    [hueWindow orderFront:nil]; 
+    [hueWindow orderFront:nil];
     visible = YES;
 }
 
+- (void) GlobalNotificationObserver:(NSNotification*)notification {
+    NSLog(@"GlobalNotify: %@", notification);
+}
+
 - (void) reposition {
-    CGFloat menuHeight = 21;
-    
     NSRect frame = [[NSScreen mainScreen] frame];
-    frame.origin.y = NSHeight(frame) - menuHeight;
-    frame.size.height = menuHeight;
-    
+    NSRect vframe = [[NSScreen mainScreen] visibleFrame];
+
+    NSLog(@"frame o=%f h=%f, vframe o=%f h=%f", frame.origin.y, frame.size.height, vframe.origin.y, vframe.size.height);
+
+    frame.origin.y = vframe.size.height + vframe.origin.y + 1;
+    frame.size.height -= (vframe.size.height + vframe.origin.y);
+
+    NSLog(@"Using %f %f", frame.origin.y, frame.size.height);
+
     [hueWindow setFrame:frame display:NO];
     [invertWindow setFrame:frame display:NO];
 }
@@ -134,18 +218,20 @@ NSString *backstop_menubar = @"Backstop Menubar";
                        ofObject:(id)object
                          change:(NSDictionary *)change
                         context:(void *)context {
-    
+
     if ( [keyPath isEqualToString:@"currentSystemPresentationOptions"] ) {
         if ( [[change valueForKey:@"new"] boolValue] ) {
             // hide
             [hueWindow orderOut:nil];
             [invertWindow orderOut:nil];
             visible = NO;
+      //      NSLog(@"currentSystemPresentationOptions -> hiding");
         } else {
             // show
             [hueWindow orderFront:nil];
             [invertWindow orderFront:nil];
             visible = YES;
+      //      NSLog(@"currentSystemPresentationOptions -> making visible");
         }
         return;
     }
@@ -156,56 +242,107 @@ NSString *backstop_menubar = @"Backstop Menubar";
                           context:context];
 }
 
-- (void) checkForFullscreen {
-    bool show = true;
-    
-
-        // Look at the windows on this screen; if we can't find the menubar backstop,
-        // we know we're in fullscreen mode
-        
-        CFArrayRef windows = CGWindowListCopyWindowInfo( kCGWindowListOptionOnScreenOnly, kCGNullWindowID );
-        CFIndex i, n;
-        
-        show = false;
-        
-        for (i = 0, n = CFArrayGetCount(windows); i < n; i++) {
-            CFDictionaryRef windict = CFArrayGetValueAtIndex(windows, i);
-            CFStringRef name = CFDictionaryGetValue(windict, kCGWindowOwnerName);
-            
-            if ([window_server compare:(NSString*)name] == 0) {                
-                name = CFDictionaryGetValue(windict, kCGWindowName);
-                if ([backstop_menubar compare:(NSString*)name] == 0) {
-                    show = true;                    
-                }
-            
-            }
-            if (show) break;
+- (void) missionControlTapped {
+    if (inMissionControl) {
+    //    NSLog(@"tapped and in mission control; show again");
+        inMissionControl = NO;
+#if USE_EVENT_TAP
+        CGEventTapEnable(eventTap, NO);
+#endif
+        if (eventMonitor) {
+            [NSEvent removeMonitor:eventMonitor];
+            eventMonitor = nil;
         }
-    
+        [self checkForFullScreen:nil];
+    }
+}
+
+- (void) missionControlActivated {
+    if (visible) {
+        [hueWindow orderOut:nil];
+        [invertWindow orderOut:nil];
+        visible = NO;
+    //    NSLog(@"missionControlActivated -> hiding");
+    }
+
+    inMissionControl = TRUE;
+#if USE_EVENT_TAP
+    CGEventTapEnable(eventTap, YES);
+#endif
+
+    eventMonitor = [NSEvent
+                    addGlobalMonitorForEventsMatchingMask:
+                        NSMouseMovedMask|NSKeyDownMask|NSLeftMouseDownMask|NSLeftMouseUpMask|
+                        NSFlagsChangedMask|NSTabletPointMask
+                    handler:^void (NSEvent *evt) {
+        [self missionControlTapped];
+    }];
+}
+
+- (void) fullScreenHideMenuBar:(NSNotification*)notification {
+    if (visible) {
+        [hueWindow orderOut:nil];
+        [invertWindow orderOut:nil];
+        visible = NO;
+    //    NSLog(@"fullScreenHideMenuBar -> hiding");
+    }
+}
+
+- (void) fullScreenShowMenuBar:(NSNotification*)notification {
+    if (!visible) {
+        [hueWindow orderFront:nil];
+        [invertWindow orderFront:nil];
+        visible = YES;
+    //    NSLog(@"fullScreenShowMenuBar -> making visible");
+    }
+}
+
+- (void) checkForFullScreen:(NSNotification*)notification {
+    bool show = true;
+
+    // Look at the windows on this screen; if we can't find the menubar backstop,
+    // we know we're in fullscreen mode
+
+    CFArrayRef windows = CGWindowListCopyWindowInfo( kCGWindowListOptionOnScreenOnly, kCGNullWindowID );
+    CFIndex i, n;
+
+    show = false;
+
+    for (i = 0, n = CFArrayGetCount(windows); i < n; i++) {
+        CFDictionaryRef windict = CFArrayGetValueAtIndex(windows, i);
+        CFStringRef name = CFDictionaryGetValue(windict, kCGWindowOwnerName);
+
+        if ([window_server compare:(NSString*)name] == 0) {
+            name = CFDictionaryGetValue(windict, kCGWindowName);
+            if ([backstop_menubar compare:(NSString*)name] == 0) {
+                show = true;
+            }
+
+        }
+        if (show) break;
+    }
+
     CFRelease(windows);
 
     if ( show && !visible ) {
         [hueWindow orderFront:nil];
         [invertWindow orderFront:nil];
         visible = YES;
+    //    NSLog(@"checkForFullScreen -> making visible");
     }
     else if ( !show && visible ) {
         [hueWindow orderOut:nil];
         [invertWindow orderOut:nil];
         visible = NO;
+    //    NSLog(@"checkForFullScreen -> hiding");
+    } else if (show) {
+        // Force them to the front again
+        [invertWindow orderFrontRegardless];
+        [hueWindow orderFrontRegardless];
+   //     NSLog(@"checkForFullScreen show + visible");
     }
 
 
-}
-
-- (void) checkForAppSwitch {
-    static int previousPid = -1;
-    int activePid = [[[[NSWorkspace sharedWorkspace] activeApplication]
-                      valueForKey:@"NSApplicationProcessIdentifier"] intValue];
-    if ( previousPid != activePid ) {
-        [self checkForFullscreen];
-        previousPid = activePid;
-    }
 }
 
 @end
